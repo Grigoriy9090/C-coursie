c0e457583ff264a931d25809f0c348ad07b1201b
“Grigoriy” <tgo90@mail.ru>
2023-08-01 11:50:42 +0300

    Последний семинар на C#

--- seminars/Program.cs
+++ seminars/Program.cs
@@ -1510,4 +1510,118 @@
 // ChangeArray();
 
 
+// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
 
+// Console.WriteLine("Введите число");//с рекурсией
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count =1;
+
+// string PrintNumber(int start, int end)
+// {
+//     if(start==end)
+//     {
+//         return start.ToString();
+//     }
+//     return (start+ " "+ PrintNumber(start+1, end));
+// }
+
+// Console.WriteLine(PrintNumber(count, num));
+
+// using System; //<без рекурсии
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("Введите число N: ");
+//         int n = int.Parse(Console.ReadLine());
+
+//         for (int i = 1; i <= n; i++)
+//         {
+//             Console.Write(i + " ");
+//         }
+
+//         Console.ReadKey();
+//     }
+// }
+
+// Задача 65: Задайте значения M и N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+// Console.WriteLine("Введите число");// с рекурсией
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumber(int start, int end)
+// {
+//     if (start>end)
+//     {
+//         return "Введите коректное число";
+//     }
+//     if(start==end)
+//     {
+//         return start.ToString();
+//     }
+//     return (start+ " "+ PrintNumber(start+1, end));
+// }
+
+// Console.WriteLine(PrintNumber(num, num2));
+
+// Задача 67: Напишите программу, которая будет 
+// принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumber(int start)
+// {
+//     if(start<0)
+//     {
+//         start=start*-1;
+//     }
+//     if(start==0)
+//     {
+//         return 0;
+//     }
+    
+//     return (start%10 + SumNumber(start/10));
+// }
+
+// Console.WriteLine(SumNumber(num));
+
+// Задача 69: Напишите программу, которая на вход
+//  принимает два числа A и B, и возводит число А
+//   в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int GetPow(int start, int rank)
+// {
+//     if(rank==0)
+//     {
+//         return 1;
+//     }
+//     if(rank==1)
+//     {
+//         return start;
+//     }
+//     return (start * GetPow(start, rank-1));
+// }
+
+// Console.WriteLine(GetPow(num,num2));
